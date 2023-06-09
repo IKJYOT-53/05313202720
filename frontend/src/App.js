24ee6741a6d5ff2577a061c570aa48505b883b18
IKJYOT-53 <ikjyot.cse1@gmail.com>
2023-06-09 21:46:19 +0530

    added cors to backend and bootstrap navbar and data to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Navbar from './Navbar';
 function App() {
   const [trains,setTrains] = useState("")
   const getTrains = async () => {
-    let result = axios.get("http://127.0.0.1:5000/trains");
+    let result = axios.get("http://localhost:5000/trains");
     result.then((result) => {
-      console.log(result)
+      setTrains(result.data)
     }).catch(err => {
       console.log(err)
     })
@@ -18,7 +19,8 @@ function App() {
   
   return (
     <div className="App">
-      {trains}
+      <Navbar />
+      {JSON.stringify(trains)}
     </div>
   );
 }
